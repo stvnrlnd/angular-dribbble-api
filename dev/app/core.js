fb162ac56e1c4b16f072147a7d494de162eae7e5
Steven Roland <steven@mojave.io>
2016-05-01 11:46:56 -0400

    Displaying Shots
    - Built a basic way to see shots from dribbble.

--- dev/app/core.js
+++ dev/app/core.js
@@ -1,4 +1,16 @@
 (function(){
   'use strict';
-  angular.module('dribbbleApp', []);
+
+  // TODO: Implement this better, preferably in a way that allows git to ignore it.
+  var accessToken = '';
+
+  angular.module('core', [])
+    .controller('shotsListController', function($scope, $http) {
+      $scope.list;
+      $http.jsonp('https://api.dribbble.com/v1/shots?callback=JSON_CALLBACK&access_token=' + accessToken)
+        .then(function(data) {
+          $scope.list = data.data;
+          console.log(data);
+        });
+    });
 })();
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 'use strict';
 // ------ Require packages
 var express = require('express');
+var bodyParser = require('body-parser');
 
 // ------ Configure the application
 var app = express();
 var port = process.env.PORT || 3000;
 var mode = 'dev' || 'build';
 
+app.use(bodyParser.json());
 app.use(express.static(mode));
 
 // ------ Build routes
